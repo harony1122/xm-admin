4143f613145659331fe0e8a55c6cddb7687fd375
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-01-10 15:21:09 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,496 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+const Canvas = require('canvas');
+const jimp = require('jimp')
+const moment = require('moment');
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`RFL Admin`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+
+
+
+
+
+
+client.on("guildMemberAdd", member => { member.createDM().then(function (channel) { return channel.send(`:rose: ولكم نورت السيرفر:rose: :crown:اسم العضو ${member}:crown: انت العضو رقم ${member.guild.memberCount} `) }).catch(console.error) })
+
+
+
+
+
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix)) return;
+  if(!message.channel.guild) return message.reply('** This command only for SeArvers **')
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    if (command === "قتل"){
+
+   var sabotage = message.mentions.users.first();
+   if(sabotage == message.author)return message.reply(`**الانتحار ما زين و الله**`);
+    if(sabotage === client.user) return message.reply(`** تبي تقتلني ؟ **`);
+  if (sabotage < 1) {
+    message.delete();
+    return message.channel.sendMessage('ضع شيئا للقتل، مثل ذكر مستخدم، أو استخدام رمز تعبيري');
+  }
+  if (!sabotage) return message.channel.send(`Please Mention A Member to Kill :warning:`)
+  message.channel.send("▄︻̷̿┻̿═━一 ${sabotage")
+  .then(msg =>{
+  msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :three:`);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :two:`);
+  }, 1000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :one:`);
+  }, 2000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 :boom:`);
+  }, 3000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 :fire:`);///Mal Team
+  }, 4000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 :skull:`);
+  }, 5000);
+  msg.delete(7876)
+  message.delete()
+  })
+  message.channel.send("**تم اخفاء الجريمة بنجاح 🕳 **").then(msg => msg.delete(10000));
+    }
+});/////////////Mal Team
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.channel.type === "dm") return;
+      if(message.content.startsWith ("زواج")) {
+              if(!message.channel.guild) return message.reply(' ');
+      if(!message.channel.guild) return message.reply(' This command only for servers ')
+      var proposed = message.mentions.members.first()
+
+      if(!message.mentions.members.first()) return message.reply('لازم تطلب ايد وحدة').catch(console.error);
+      if(message.mentions.users.size > 1) return message.reply('ولد ما يضبط لازم بنت تذكر لازم بنت الحلال').catch(console.error);
+       if(proposed === message.author) return message.reply(`**خنثى ؟ **`);
+        if(proposed === client.user) return message.reply(`** تبي تتزوجني؟ **`);
+              message.channel.send(`**${proposed} 
+ بدك تقبلي عرض الزواج من ${message.author}
+ العرض لمدة 10 ثانية 
+ اكتب موافقة او لا**`)
+
+const filter = m => m.content.startsWith("موافقة");
+message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
+.then(collected =>{ 
+    message.channel.send(`**${message.author} و ${proposed} الف الف مبروك انشاء الله تستمتعون بحياتكم الزوجية ويطول اعماركم ولا تنسون شهر العسل**`);
+})
+   .catch(collected => message.channel.send(`**السكوت علامة الرضا.قولو مبروك**`))
+
+   const filte = m => m.content.startsWith("لا");
+message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
+.then(collected =>{ 
+   message.channel.send(`**${message.author} تم رفض عرضك**`);
+})
+
+
+
+
+  }
+});///////////Mal Team
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+  if(message.content.startsWith(prefix + "invites")) {
+   message.guild.fetchInvites().then(invs => {
+     let user = message.mentions.users.first() || message.author
+     let personalInvites = invs.filter(i => i.inviter.id === user.id);
+     let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+ }
+});
+
+
+  
+
+
+
+
+var guilds = {};
+client.on('guildBanAdd', function(guild) {
+            const rebellog = client.channels.find("name", "log"),
+            Onumber = 10,
+  Otime = 10000
+guild.fetchAuditLogs({
+    type: 22
+}).then(audit => {
+    let banner = audit.entries.map(banner => banner.executor.id)
+    let bans = guilds[guild.id + banner].bans || 0
+    guilds[guild.id + banner] = {
+        bans: 0
+    }
+      bans[guilds.id].bans += 3;
+if(guilds[guild.id + banner].bans >= Onumber) {
+try {
+let roles = guild.members.get(banner).roles.array();
+guild.members.get(banner).removeRoles(roles);
+
+} catch (error) {
+console.log(error)
+try {
+guild.members.get(banner).removeRoles(roles);
+  rebellog.send(`<@!${banner.id}>
+حآول العبث بالسيرفر @everyone`);
+guild.owner.send(`<@!${banner.id}>
+حآول العبث بالسيرفر ${guild.name}`)
+    setTimeout(() => {
+ guilds[guild.id].bans = 0;
+  },Otime)
+} catch (error) {
+console.log(error)
+}
+}
+}
+})
+});
+ let channelc = {};
+  client.on('channelCreate', async (channel) => {
+  const rebellog = client.channels.find("name", "log"),
+  Oguild = channel.guild,
+  Onumber = 10,
+  Otime = 10000;
+  const audit = await channel.guild.fetchAuditLogs({limit: 1});
+  const channelcreate = audit.entries.first().executor;
+  console.log(` A ${channel.type} Channel called ${channel.name} was Created By ${channelcreate.tag}`);
+   if(!channelc[channelcreate.id]) {
+    channelc[channelcreate.id] = {
+    created : 0
+     }
+ }
+ channelc[channelcreate.id].created += 3;
+ if(channelc[channelcreate.id].created >= Onumber ) {
+let roles = guild.members.get(banner).roles.array();
+guild.members.get(banner).removeRoles(roles);
+rebellog.send(`<@!${channelcreate.id}>
+حآول العبث بالسيرفر @everyone`);
+channel.guild.owner.send(`<@!${channelcreate.id}>
+حآول العبث بالسيرفر ${channel.guild.name}`)
+}
+  setTimeout(() => {
+ channelc[channelcreate.id].created = 0;
+  },Otime)
+  });
+
+let channelr = {};
+  client.on('channelDelete', async (channel) => {
+  const rebellog = client.channels.find("name", "log"),
+  Oguild = channel.guild,
+  Onumber = 10,
+  Otime = 10000;
+  const audit = await channel.guild.fetchAuditLogs({limit: 1});
+  const channelremover = audit.entries.first().executor;
+  console.log(` A ${channel.type} Channel called ${channel.name} was deleted By ${channelremover.tag}`);
+   if(!channelr[channelremover.id]) {
+    channelr[channelremover.id] = {
+    deleted : 0
+     }
+ }
+ channelr[channelremover.id].deleted += 3;
+ if(channelr[channelremover.id].deleted >= Onumber ) {
+let roles = guild.members.get(banner).roles.array();
+guild.members.get(banner).removeRoles(roles);
+rebellog.send(`<@!${channelremover.id}>
+حآول العبث بالسيرفر @everyone`);
+channel.guild.owner.send(`<@!${channelremover.id}>
+حآول العبث بالسيرفر ${channel.guild.name}`)
+}
+  setTimeout(() => {
+ channelr[channelremover.id].deleted = 0;
+  },Otime)
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('guildMemberAdd', (member) => {
+member.addRole(member.guild.roles.find('name', '『⊰ʀғ⍸』'));  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", message => {
+    var prefix = "c";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©KBOOSHDev"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
+
+
+
+
+  
+ 
+
+
+client.on("ready", async  => {
+setInterval(function(){
+
+client.channels.find('id', '524087588458594305').setName("W");
+client.channels.find('id', '524087588458594305').setName("We");
+client.channels.find('id', '524087588458594305').setName("Wel");
+client.channels.find('id', '524087588458594305').setName("Welc");
+client.channels.find('id', '524087588458594305').setName("Welco");
+client.channels.find('id', '524087588458594305').setName("Welcom");
+client.channels.find('id', '524087588458594305').setName("Welcome");
+client.channels.find('id', '524087588458594305').setName("Welcome T");
+client.channels.find('id', '524087588458594305').setName("Welcome To");
+client.channels.find('id', '524087588458594305').setName("Welcome To ");
+client.channels.find('id', '524087588458594305').setName("Welcome To  ");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R ");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F ");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F L");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F L.."); /// 
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F L..");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F L..");
+
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F L..");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F ");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R F");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R ");
+client.channels.find('id', '524087588458594305').setName("Welcome To  R");
+client.channels.find('id', '524087588458594305').setName("Welcome To  ");
+client.channels.find('id', '524087588458594305').setName("Welcome To ");
+client.channels.find('id', '524087588458594305').setName("Welcome To");
+client.channels.find('id', '524087588458594305').setName("Welcome T");
+client.channels.find('id', '524087588458594305').setName("Welcome ");
+client.channels.find('id', '524087588458594305').setName("Welcome");
+client.channels.find('id', '524087588458594305').setName("Welcom");
+client.channels.find('id', '524087588458594305').setName("Welco");
+client.channels.find('id', '524087588458594305').setName("Welc");
+client.channels.find('id', '524087588458594305').setName("Wel");
+client.channels.find('id', '524087588458594305').setName("We");
+client.channels.find('id', '524087588458594305').setName("W");
+
+  
+  }, 4000);
+
+});
+
+
+
+
+
+
+
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+        if(!message.channel.guild) return;
+        message.delete()
+    return message.reply(`** ممنوع نشر السيرفرات سوف يتم معاقبتك😠 ! **`)
+    }
+});
+
+
+
+
+
+
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('508761735365263370').roles.find('name', 'color').edit({color: 'RANDOM'})
+    },5000);
+  
+
+})
+
+
+
+
+
+
+
+
+
+const ms = require("ms");
+
+const fs = require('fs');
+
+var user = {};
+var warn = {};
+
+client.on('message', function(message) {
+
+    	 if (!message.channel.guild) return;
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+
+  if (message.author.id == client.user.id) return;
+  if(JSON.stringify(user).indexOf(message.author.id) == -1) {
+    user[message.author.id] = message.createdTimestamp;
+    return;
+  } else {
+    if (Date.now() - user[message.author.id] < 695){
+              message.author.delete
+
+      if (JSON.stringify(warn).indexOf(message.author.id) == -1) {
+        warn[message.author.id] = 1;
+      } else {
+        warn[message.author.id]++;
+        message.author.delete
+      }
+      if (warn[message.author.id] < 4) {
+        message.author.delete
+
+      }
+      delete user[message.author.id];
+              message.author.delete
+
+    } else {
+      delete user[message.author.id];
+              message.author.delete
+
+    }
+  }
+  if (warn[message.author.id] == 4) {		   
+     if (!message.channel.guild) return;
+             message.author.delete
+
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+          var currentTime = new Date(),
+                   Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate(),
+hours = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes()+1,
+            seconds = currentTime.getSeconds();
+
+           if (!message.channel.guild) return;
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+    message.guild.members.get(message.author.id).addRole(muteRole1);
+    
+     var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+
+delete warn[message.author.id];
+    delete user[message.author.id];
+	const embed500 = new Discord.RichEmbed()
+     .setTitle(`المرسل ${message.author.username}#${message.author.discriminator} `)
+      .setDescription(":white_check_mark:  | `محاولة السبام`\n\nالاسم:\n"+`${message.author.username}#${message.author.discriminator}`+"\nالعقوبة:\n  MuteChat / ميوت كتابي\n")
+      .setFooter("Anti - Spam")
+      .setColor("c91616")
+    message.channel.send(embed500)
+    	const embed20 = new Discord.RichEmbed()
+      .setTitle(":scales: | تمت معاقبتك")
+      .setDescription(`**:small_blue_diamond:لقد قمت بمخالفة قوانين السيرفر**\n \n:gun: : نوع العقوبه\nMuteChat / ميوت كتابي\n:clock1: وقت وتاريخ العقوبه:\n`+ Year + "/" + Month + "/" + Day +', '+hours +'-' +minutes+'-'+seconds+"\n \n \n`في حال كانت العقوبة بالغلط, تواصل مع الادارة`")
+          .setFooter("Anti - Spam")
+      .setColor("c91616")
+    
+     message.author.send(embed20)
+  
+  }
+});
+
+
+
+
+
+
+
+
+client.login("NTIzOTA2NDE2NTU4NDczMjE3.DvgVwQ.wq-_rnT3-hVji_YsDXBT5ntnnsU");
